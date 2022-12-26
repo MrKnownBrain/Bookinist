9e8bc6280526310b8806e9c43de7529fce88ca85
Andrey MrknownBrain <MrKnowForeverBrain@yandex.ru>
2022-12-26 12:23:52 +0300

    Наведение порядка в структуре

--- Bookinist/Views/Windows/MainWindow.xaml.cs
+++ Bookinist/Views/Windows/MainWindow.xaml.cs
@@ -10,13 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Bookinist
+namespace Bookinist.Views.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
