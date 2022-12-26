81ff0491ed4d99606525c7b35b5564671b095068
Andrey MrknownBrain <MrKnowForeverBrain@yandex.ru>
2022-12-26 13:56:06 +0300

    Конвейер сервисов

--- Bookinist/App.xaml.cs
+++ Bookinist/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using System;
 using System.Windows;
 
 namespace Bookinist
@@ -13,5 +11,26 @@ namespace Bookinist
     /// </summary>
     public partial class App : Application
     {
+        private static IHost __Host;
+        public static IHost Host => __Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
---
+++ Bookinist/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using System;
+
+namespace Bookinist
+{
+    class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureServices(App.ConfigureServices);
+    }
+}
